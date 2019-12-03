275d7f3aa2d9070cec952f2cd33387eecdc83ce5
hokolomopo <adrien.minne@outlook.com>
2019-12-03 15:41:57 +0100

    Improve quickplay.dart modularity

--- lib/quick_play.dart
+++ lib/quick_play.dart
@@ -4,24 +4,9 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert'; // json codec
 import 'package:info2051_2018/game/sound_player.dart';
+import 'package:info2051_2018/quickplay_widgets.dart';
 
 import 'game/game_main.dart';
-
-class Terrain {
-  final String name;
-  final String imgName;
-  String levelObject;
-
-  Terrain({this.name, this.imgName, this.levelObject});
-
-  factory Terrain.fromJson(Map<String, dynamic> json) {
-    return new Terrain(
-      name: json['name'] as String,
-      imgName: json['imgName'] as String,
-      levelObject: json['levelObject'] as String,
-    );
-  }
-}
 
 class Parameters extends StatefulWidget {
   final void Function() parentAction;
@@ -56,8 +41,8 @@ class ParametersState extends State<Parameters> {
     });
   }
 
-
-  void _handleTerrain(Terrain value) {
+  void _selectTerrain(Terrain value) {
+    print("selectTerrain");
     setState(() {
       _terrain = value;
     });
@@ -82,93 +67,27 @@ class ParametersState extends State<Parameters> {
     return parseJson(tmp.toString());
   }
 
-  Widget customTerrainRadioList(List<Terrain> terrains) {
-    return SingleChildScrollView(
-      physics: AlwaysScrollableScrollPhysics(),
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        height: 200.0,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          shrinkWrap: true,
-          itemCount: terrains == null ? 0 : terrains.length,
-          itemBuilder: (context, index) {
-            return customTerrainRadio(terrains[index]);
-          },
-        ),
-      ),
-    );
-  }
+  List<Widget> buildRadioList({String title, List<String> texts, List<Color> colors, int groupValue, Function onChanged}){
+    List<Widget> list = List();
+    print("lllh");
+    list.add(Expanded(
+        child: FittedBox(
+            fit: BoxFit.contain,
+            child: Text(title))));
 
-  Widget customTerrainRadio(Terrain terrain) {
-    if (_terrain != null && terrain.name == _terrain.name)
-      return Container(
-        width: 200,
-        height: 200,
-        decoration: BoxDecoration(
-            border: Border.all(color: Colors.red, width: 3.0),
-            image: DecorationImage(
-              image:
-                  AssetImage('assets/graphics/backgrounds/' + terrain.imgName),
-              fit: BoxFit.fill,
-            )),
-        child: InkWell(
-          onTap: () {
-            _handleTerrain(terrain);
-          },
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: AutoSizeText(
-                      terrain.name,
-                      maxLines: 1,
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      );
-    else
-      return Container(
-        width: 200,
-        height: 200,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage('assets/graphics/backgrounds/' + terrain.imgName),
-          fit: BoxFit.fill,
-        )),
-        child: InkWell(
-          onTap: () {
-            _handleTerrain(terrain);
-          },
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: AutoSizeText(
-                      terrain.name,
-                      maxLines: 1,
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      );
+    for(int i = 0;i < texts.length; i++){
+      list.add(Expanded(
+          child: Column(children: <Widget>[
+            Text(texts[i]),
+            Radio(
+              activeColor: colors[i],
+              value: i,
+              groupValue: groupValue,
+              onChanged: onChanged,
+            )
+          ])));
+    }
+    return list;
   }
 
   Widget _parameter(BuildContext context) {
@@ -177,99 +96,24 @@ class ParametersState extends State<Parameters> {
       children: <Widget>[
         Padding(
           padding: EdgeInsets.only(bottom: 10.0, top: 20.0),
-          child: Row(children: <Widget>[
-            Expanded(
-                child: FittedBox(
-                    fit: BoxFit.contain, child: Text("Number\nPlayers"))),
-            Expanded(
-                child: Column(children: <Widget>[
-              Text("1P"),
-              Radio(
-                activeColor: Colors.redAccent,
-                value: 1,
-                groupValue: _nbPlayer,
-                onChanged: _handleNbPlayers,
-              )
-            ])),
-            Expanded(
-                child: Column(children: <Widget>[
-              Text("2P"),
-              Radio(
-                activeColor: Colors.lightBlueAccent,
-                value: 2,
-                groupValue: _nbPlayer,
-                onChanged: _handleNbPlayers,
-              )
-            ])),
-            Expanded(
-                child: Column(children: <Widget>[
-              Text("3P"),
-              Radio(
-                activeColor: Colors.green,
-                value: 3,
-                groupValue: _nbPlayer,
-                onChanged: _handleNbPlayers,
-              )
-            ])),
-            Expanded(
-                child: Column(children: <Widget>[
-              Text("4P"),
-              Radio(
-                activeColor: Colors.deepPurpleAccent,
-                value: 4,
-                groupValue: _nbPlayer,
-                onChanged: _handleNbPlayers,
-              )
-            ]))
-          ]),
+          child: Row(children: buildRadioList(
+            title: "Number\nPlayers",
+            texts: ["1P", "2P", "3P", "4P"],
+            colors: [Colors.redAccent, Colors.lightBlueAccent, Colors.green, Colors.deepPurpleAccent],
+            groupValue: _nbPlayer,
+            onChanged: _handleNbPlayers
+          )),
         ),
         Padding(
             padding: EdgeInsets.only(bottom: 10.0, top: 10.0),
-            child: Row(children: <Widget>[
-              Expanded(
-                  child: FittedBox(
-                      fit: BoxFit.contain, child: Text("Number\nWorms"))),
-              Expanded(
-                  child: Column(children: <Widget>[
-                Text("1"),
-                Radio(
-                  activeColor: Colors.redAccent,
-                  value: 1,
-                  groupValue: _nbWorms,
-                  onChanged: _handleNbWorms,
-                )
-              ])),
-              Expanded(
-                  child: Column(children: <Widget>[
-                Text("2"),
-                Radio(
-                  activeColor: Colors.lightBlueAccent,
-                  value: 2,
-                  groupValue: _nbWorms,
-                  onChanged: _handleNbWorms,
-                )
-              ])),
-              Expanded(
-                  child: Column(children: <Widget>[
-                Text("3"),
-                Radio(
-                  activeColor: Colors.green,
-                  value: 3,
-                  groupValue: _nbWorms,
-                  onChanged: _handleNbWorms,
-                )
-              ])),
-              Expanded(
-                  child: Column(children: <Widget>[
-                Text("4"),
-                Radio(
-                  activeColor: Colors.deepPurpleAccent,
-                  value: 4,
-                  groupValue: _nbWorms,
-                  onChanged: _handleNbWorms,
-                )
-              ]))
-            ])),
+            child: Row(children: buildRadioList(
+              title: "Number\nWorms",
+              texts: ["1", "2", "3", "4"],
+              colors: [Colors.redAccent, Colors.lightBlueAccent, Colors.green, Colors.deepPurpleAccent],
+              groupValue: _nbWorms,
+              onChanged: _handleNbWorms
+          ))
+        ),
         SizedBox(
           height: 20.0,
         ),
@@ -280,7 +124,11 @@ class ParametersState extends State<Parameters> {
             future: getTerrainInfo(),
             builder: (context, snapshot) {
               return (snapshot.data != null)
-                  ? customTerrainRadioList(snapshot.data)
+                  ? TerrainScrollableList(
+                    snapshot.data,
+                    selectedTerrain: _terrain,
+                    onTap: _selectTerrain,
+                   )
                   : Center(
                       child: CircularProgressIndicator(),
                     );
