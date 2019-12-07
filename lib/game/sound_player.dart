e2f3f370b104bf15ecb5bedb0482ece92de5622f
L'hoest Julien <j.lhoest@student.uliege.be>
2019-12-07 17:28:47 +0100

    audio small correction
    There is still an issue with the music in background

--- lib/game/sound_player.dart
+++ lib/game/sound_player.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
@@ -8,30 +7,33 @@ import 'package:flutter/services.dart';
 class SoundPlayer {
   AudioPlayer audioPlayer;
 
-  SoundPlayer() {
-    this.audioPlayer = AudioPlayer(mode: PlayerMode.MEDIA_PLAYER);
-
+  SoundPlayer(bool lowLatencyMode) {
+    if (lowLatencyMode)
+      this.audioPlayer = AudioPlayer(mode: PlayerMode.LOW_LATENCY);
+    else
+      this.audioPlayer = AudioPlayer(mode: PlayerMode.MEDIA_PLAYER);
   }
 
-  void playLocalMusic(String assetPath, String id) async {
+  void playLocalAudio(String assetPath, String id, bool isSoundEffect) async {
     final file = new File('${(await getTemporaryDirectory()).path}/' + id);
-    await file.writeAsBytes((await rootBundle.load(assetPath)).buffer.asUint8List());
-    await this.audioPlayer.setReleaseMode(ReleaseMode.LOOP);
-    await this.audioPlayer.play( file.path, isLocal: true, stayAwake: true);
+    await file
+        .writeAsBytes((await rootBundle.load(assetPath)).buffer.asUint8List());
+
+    if(isSoundEffect){
+      await this.audioPlayer.setReleaseMode(ReleaseMode.RELEASE);
+      await this.audioPlayer.play(file.path, isLocal: true);
+    }
+    else{
+      await this.audioPlayer.setReleaseMode(ReleaseMode.LOOP);
+      await this.audioPlayer.play(file.path, isLocal: true, stayAwake: true);
+    }
   }
 
-  void playLocalSoundEffect(String assetPath, String id) async{
-    final file = new File('${(await getTemporaryDirectory()).path}/' + id);
-    await file.writeAsBytes((await rootBundle.load(assetPath)).buffer.asUint8List());
-    await this.audioPlayer.setReleaseMode(ReleaseMode.RELEASE);
-    await this.audioPlayer.play( file.path, isLocal: true);
-  }
-
-  void pause() async{
+  void pause() async {
     await this.audioPlayer.pause();
   }
 
-  void resume() async{
+  void resume() async {
     await this.audioPlayer.resume();
   }
 
@@ -42,5 +44,4 @@ class SoundPlayer {
   void release() async {
     await this.audioPlayer.release();
   }
-
 }
